4f94a19a4724516a4b8e82fbe9bfa39bbc1511db
UnCoqui <richard.d.merced@gmail.com>
2023-06-06 13:39:16 -0700

    added original TTTCss file and script.js file

---
+++ script.js
@@ -0,0 +1,6 @@
+//Enter your Javascript code here. You must rename this file with your initials for your first name and last name.
+// Example John Doe will be jdscript.js
+
+// Instructions from https://www.codebrainer.com/blog/tic-tac-toe-javascript-game
+
+
